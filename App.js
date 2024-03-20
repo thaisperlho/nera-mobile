973c02d84afa7174c293237defc1a6855bffafaa
Thaís Carvalho <thaiscarvalho@Thaiss-MacBook-Air.local>
2024-03-20 17:10:22 -0300

    build(deps): update 3 files and create 1 file create README.md and Icone.png

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import styles from './styles';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Nera</Text>
+      <Image source={require('./assets/Icone.png')} style={styles.img}/>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    img: {
+        width: 100,
+        height: 100,
+    }
+});
+export default styles;
