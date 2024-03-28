c9131a500c95f16d503d72d5352355bd51ae3ab0
amandaSribeiro <amanda.524.ribeiro@gmail.com>
2024-03-28 19:52:28 -0300

    Criação da tela de splash

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import styles from './styles';
-
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import SplashScreen from './components/SplashScreen';
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Nera</Text>
-      <Image source={require('./assets/Icone.png')} style={styles.img}/>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Splash" headerMode="none">
+        <Stack.Screen name="Splash" component={SplashScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+}; 
